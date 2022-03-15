60d9a8eee99ab5e50e2c01526a2671f277d26637
lily4499 <konissil@gmail.com>
2022-03-15 16:16:45 -0400

    portfolio

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const hamburgerButton = document.getElementById('hamburger')
+/* const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 
 function toggleButton() {
     navList.classList.toggle('show')
 }
 
-hamburgerButton.addEventListener('click', toggleButton)
+hamburgerButton.addEventListener('click', toggleButton) */
